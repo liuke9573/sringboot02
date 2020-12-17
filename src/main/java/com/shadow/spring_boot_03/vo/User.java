94df7583f40a93159859fff2270ed6fdda84e11a
shadow <471744338@qq.com>
2020-12-17 19:29:31 +0800

    springboot03 推送学习

---
+++ src/main/java/com/shadow/spring_boot_03/vo/User.java
@@ -0,0 +1,32 @@
+package com.shadow.spring_boot_03.vo;
+
+public class User {
+
+    private Integer id;
+    private String userName;
+    private String userPwd;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+}
